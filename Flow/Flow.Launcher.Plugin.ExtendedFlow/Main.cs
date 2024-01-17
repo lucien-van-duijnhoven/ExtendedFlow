3d3ec9198fbf4f12dd3aabb9bd5cc09a2fcbde33
Lucien van Duijnhoven <lucien.v.d@outlook.com>
2024-01-17 16:37:01 +0100

    Flow api calls

--- Flow/Flow.Launcher.Plugin.ExtendedFlow/Main.cs
+++ Flow/Flow.Launcher.Plugin.ExtendedFlow/Main.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using Flow.Launcher.Plugin;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Security;
 
 namespace Flow.Launcher.Plugin.ExtendedFlow
 {
     public class ExtendedFlow : IPlugin
     {
         private PluginInitContext _context;
+        private readonly HttpClient _client = new();
 
         public void Init(PluginInitContext context)
         {
@@ -15,27 +19,46 @@ namespace Flow.Launcher.Plugin.ExtendedFlow
 
         public List<Result> Query(Query query)
         {
+
+            // Make a http request to the server
+
             return new List<Result>
             {
-                new Result
+                new ()
                 {
                     Title = "Toggle siteblock",
                     SubTitle = "Reroute all the blocked sites to a black page",
                     Action = _ =>
                     {
+                        ToggleSiteBlock();
                         return true;
                     }
                 },
-                new Result
+                new()
                 {
                     Title = "Toggle monochrome",
                     SubTitle = "Turn on/off monochrome mode",
                     Action = _ =>
                     {
+                        ToggleMonochrome();
                         return true;
                     }
-                },
+                }
             };
+        }
+
+        private async void ToggleMonochrome()
+        {
+            var response = await _client.GetAsync("http://localhost:4000/grayscale");
+            response.EnsureSuccessStatusCode();
+            response.Dispose();
+        }
+
+        private async void ToggleSiteBlock()
+        {
+            var response = await _client.GetAsync("http://localhost:4000/block");
+            response.EnsureSuccessStatusCode();
+            response.Dispose();
         }
     }
 }
